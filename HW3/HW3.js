42defdeebb8e36258c8e0d4fc9d3f682bfa3be7b
Yiran Buckley <ybuckley02@gmail.com>
2016-10-08 10:31:15 -0400

    jQuery elements added

--- HW3/HW3.js
+++ HW3/HW3.js
@@ -3,6 +3,14 @@ console.log("Hi");
 function buttonclicked(){
 	var wholelist = document.getElementById("list");
 
+	var taskinput = $("input:text").val();
+	console.log(taskinput);
+	var check = $("<input name='checking' type='checkbox' class='pull-right'>");
+
+	$("#list")
+		.append($("<li>").text(taskinput).append(check));
+
+	/*
 	var eatapple = document.getElementById("taskinput").value;
 	var checkbox = document.createElement("input");
 	checkbox.type = "checkbox"
@@ -10,11 +18,13 @@ function buttonclicked(){
 	
 	var entry = document.createElement("li");
 
-	//document.getElementById("taskinput").value ="bwork yourself";
+	
+
+
 
 	entry.appendChild(document.createTextNode(eatapple))
 	entry.appendChild(checkbox)
 	entry.className = "list-group-item";
-	wholelist.appendChild(entry);	
+	wholelist.appendChild(entry);	*/
 
 }
